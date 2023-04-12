46b7aae1c4a27df1a52ce4a8c468ce69f9d94530
Nicole Gottschall <ngottschall@web.de>
2023-04-12 09:46:30 +0200

    loops through array

--- pr02/PR02/Program.cs
+++ pr02/PR02/Program.cs
@@ -13,7 +13,7 @@ namespace PR01_ConsoleApp
 
         static void Main(string[] args)
         {
-          
+
             Console.WriteLine("===================");
 
             Console.WriteLine("Berechnung Flächeninhalt eines Rechtecks:");
@@ -39,16 +39,29 @@ namespace PR01_ConsoleApp
 
             Console.WriteLine("Erstelle Personen...");
 
-            Person p1 = new Person("Mustermann", "Max", 21, "Mittweida", "Technikumplatz 17", "23.03.2002");
+            Person[] personen = new Person[]
+            {
+                new Person("Mustermann", "Max", 21, "Mittweida", "Technikumplatz 17", "23.03.2002"),
+                new Person("Müller", "Claudia", 23, "Chemnitz", "Moritzstrasse 32", "23.07.2000"),
+                new Person("Mustermann", "Max", 21, "Mittweida", "Technikumplatz 17", "23.03.2002"),
+            };
+                
+            Console.WriteLine(personen.Length + " Person(en) wurde(n) erstellt:");
 
-            Console.WriteLine("1 Person wurde erstellt:");
-
-            Console.WriteLine(p1.Vorname);
-            p1.Vorname = "Moriz";
-            Console.WriteLine(p1.Vorname);
+            for(int i = 0; i < personen.Length; i++)
+            {
+                Console.WriteLine("Person " + i + ":");
+                Console.WriteLine(
+                    "Name, Vorname: " + personen[i].Name + ", " + personen[i].Vorname + "\n"
+                    + "Geburtsdatum, Alter: " + personen[i].Geburtsdatum + ", " + personen[i].Alter + "\n"
+                    + "Adresse: " + personen[i].Wohnort + ", " + personen[i].Adresse + "\n");
+            }
 
             Console.WriteLine("Press Key to close CMD.");
             Console.ReadKey(); //prevents CMD from closing automatically after running
+
+            ///TODO: Moegliche Erweiterung: Hinzufuegen erstellter Personen zu Array
+            ///und Ausgabe der erstellten Personen, indem Array durchlaufen wird
         }
 
         /// <summary>
